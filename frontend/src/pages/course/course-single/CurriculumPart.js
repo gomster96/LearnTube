1d59e54c5fca58355d0227ea0f555eb3a989e8d0
JuEun <moomin@handong.ac.kr>
2022-05-06 04:40:02 +0900

    [fe/feat/#36] feat: 공지 생성 구현중

--- frontend/src/pages/course/course-single/CurriculumPart.js
+++ frontend/src/pages/course/course-single/CurriculumPart.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import ModalVideo from "react-modal-video";
 import Modal from "react-modal";
+import axios from "axios";
+
 import {
   Accordion,
   AccordionItem,
@@ -9,17 +11,65 @@ import {
   AccordionItemPanel,
   AccordionItemButton,
 } from "react-accessible-accordion";
+import { faBorderNone } from "@fortawesome/free-solid-svg-icons";
+
 // import { Button, Modal } from "react-bootstrap";
 const CurriculumPart = (props) => {
+  const inputRef = useRef();
   const [isOpen, setIsOpen] = useState(false);
   const openModal = () => setIsOpen(!isOpen);
+  const [isNoticeCreateOpen, setNoticeCreateOpen] = useState(false);
+  const createNotice = () => setNoticeCreateOpen(!isNoticeCreateOpen);
+  const cancleCreateNotice = () => setNoticeCreateOpen(!isNoticeCreateOpen);
   const [noticeIdx, setNoticeIdx] = useState(0);
-  console.log("hello ", props.classRoomData);
+  console.log("hello id ", props.classRoomData.classId);
   const clickModalHandler = (params) => {
     console.log("hello ", params);
     setNoticeIdx(params);
     console.log("not: ", noticeIdx);
   };
+  const [noticeTitle, setNoticeTitle] = useState("");
+  const [noticeContent, setNoticeContent] = useState("");
+
+  const noticeCancelHandler = () => {
+    setNoticeTitle("");
+    setNoticeContent("");
+  };
+
+  const noticeTitleHandler = (e) => {
+    console.log(e.target.value);
+    setNoticeTitle(e.target.value);
+  };
+
+  const noticeContentHandler = (e) => {
+    console.log(e.target.value);
+    setNoticeContent(e.target.value);
+  };
+
+  const noticeCreateHandler = (e) => {
+    // console.log(e.target.value);
+    // state에 저장한 값을 가져옵니다.
+    console.log(noticeTitle);
+    console.log(noticeContent);
+
+    let body = {
+      classId: props.classRoomData.classId,
+      title: noticeContent,
+      content: noticeContent,
+      //modDate:
+    };
+
+    axios
+      .post("http://localhost:8080/api/notice", JSON.stringify(body), {
+        headers: {
+          "Content-Type": `application/json`,
+        },
+      })
+      .then((res) => console.log(res));
+  };
+  useEffect(() => {
+    console.log("값 변화");
+  }, [noticeIdx]);
 
   return (
     <>
@@ -32,7 +82,169 @@ const CurriculumPart = (props) => {
             <AccordionItem className="accordion-item" uuid="a">
               <AccordionItemHeading>
                 <AccordionItemButton>
-                  <button>공지</button>
+                  <Modal
+                    isOpen={isNoticeCreateOpen}
+                    onClose={() => {
+                      createNotice();
+                    }}
+                    onRequestClose={() => setNoticeCreateOpen(false)}
+                    style={{
+                      overlay: {
+                        zIndex: "100",
+                        position: "fixed",
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        backgroundColor: "rgb(0, 0, 0, 0.55)",
+                      },
+                      content: {
+                        position: "absolute",
+                        top: "20%",
+                        left: "25%",
+                        right: "25%",
+                        bottom: "100px",
+                        background: "#fff",
+                        overflow: "auto",
+                        WebkitOverflowScrolling: "touch",
+                        outline: "none",
+                        padding: "0px",
+                      },
+                    }}
+                  >
+                    <div className="">
+                      <div className="register-section ">
+                        <div className="container">
+                          <div className="py-3 px-5">
+                            <div className="sec-title text-center mb-10">
+                              <h2 className="title mt-3 mb-10">공지 생성</h2>
+                            </div>
+                            <div className="styled-form">
+                              <div id="form-messages"></div>
+                              <form id="contact-form" method="post" action="#">
+                                <div className="row clearfix">
+                                  <div className="form-group col-lg-12 mb-25">
+                                    <div className="my-4">
+                                      공지 제목
+                                      <span
+                                        className="ms-1"
+                                        style={{ color: "red" }}
+                                      >
+                                        *
+                                      </span>
+                                    </div>
+                                    <input
+                                      type="text"
+                                      id="title"
+                                      name="title"
+                                      value={noticeTitle}
+                                      onChange={noticeTitleHandler}
+                                      placeholder="제목을 입력하세요"
+                                      required
+                                    />
+                                  </div>
+                                  <div className="form-group col-lg-12">
+                                    <div className="my-4">공지 내용</div>
+
+                                    <textarea
+                                      type="textarea"
+                                      id="description"
+                                      name="description"
+                                      value={noticeContent}
+                                      onChange={noticeContentHandler}
+                                      placeholder="설명을 입력하세요"
+                                      refs={inputRef}
+                                      style={{
+                                        position: "relative",
+                                        borderRadius: "0px",
+                                        padding: "6px 30px",
+                                        width: "100%",
+                                        color: "#222222",
+                                        fontSize: "16px",
+                                        transition: "all 500ms ease",
+                                        border: "none",
+                                        boxShadow: "0 0 30px #eee",
+                                      }}
+                                    />
+                                  </div>
+                                  {/* <div className="form-group col-lg-12">
+                                    <div className="my-2">Playlist 태그</div>
+                                    <input
+                                      type="text"
+                                      id="tag"
+                                      name="tag"
+                                      value=""
+                                      placeholder="태그를 입력하세요. 쉼표로 구분됩니다."
+                                    />
+                                  </div> */}
+                                </div>
+                                <div className="row d-flex justify-content-end ms-3 me-1 mt-3">
+                                  <button
+                                    type="submit"
+                                    className="canclebtn"
+                                    onClick={() => {
+                                      cancleCreateNotice();
+                                      noticeCancelHandler();
+                                    }}
+                                  >
+                                    <span className="txt">취소</span>
+                                  </button>
+                                  <button
+                                    type="submit"
+                                    className="createbtn text-center pt-2"
+                                    onClick={() => {
+                                      createNotice();
+                                      noticeCreateHandler();
+                                    }}
+                                  >
+                                    저장
+                                  </button>
+                                </div>
+                              </form>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </Modal>
+                  <button
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <div
+                      style={{
+                        display: "flex",
+                      }}
+                    >
+                      공지
+                    </div>
+
+                    <div
+                      style={{
+                        display: "flex",
+                        marginRight: "25px",
+                      }}
+                    >
+                      <span
+                        onClick={() => {
+                          createNotice();
+                        }}
+                      >
+                        <i
+                          className="fa fa-plus p-1"
+                          id="createNotice"
+                          style={{
+                            display: "flex",
+                            padding: "3px",
+                            zIndex: "0",
+                          }}
+                        ></i>
+                      </span>
+                    </div>
+                  </button>
                 </AccordionItemButton>
               </AccordionItemHeading>
               <AccordionItemPanel className="card-body acc-content current">
@@ -154,11 +366,36 @@ const CurriculumPart = (props) => {
                             <i
                               className="fa fa-list"
                               style={{ zIndex: "0" }}
-                            ></i>{" "}
+                            ></i>
                             {props.classRoomData.notices[i].title}
                           </div>
+
                           <div className="pull-right">
-                            <div className="minutes"></div>
+                            <div className="minutes">
+                              <div>
+                                <span onClick={createNotice}>
+                                  <i
+                                    className="fa fa-edit"
+                                    id="editNotice"
+                                    style={{
+                                      padding: "5px",
+
+                                      zIndex: "0",
+                                    }}
+                                  ></i>
+                                </span>
+                                <span onClick={createNotice}>
+                                  <i
+                                    className="fa fa-trash"
+                                    style={{
+                                      padding: "5px",
+
+                                      zIndex: "0",
+                                    }}
+                                  ></i>
+                                </span>
+                              </div>
+                            </div>
                           </div>
                         </div>
                       </div>
@@ -172,9 +409,58 @@ const CurriculumPart = (props) => {
                   <AccordionItem className="accordion-item" uuid="c">
                     <AccordionItemHeading>
                       <AccordionItemButton>
-                        <button>
-                          {" "}
-                          {props.classRoomData.lectures[i].lectureNum}강
+                        <button
+                          style={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "space-between",
+                          }}
+                        >
+                          <div
+                            style={{
+                              display: "flex",
+                            }}
+                          >
+                            {props.classRoomData.lectures[i].lectureNum}강
+                          </div>
+
+                          <div
+                            style={{
+                              display: "flex",
+                              marginRight: "25px",
+                            }}
+                          >
+                            <span onClick={openModal}>
+                              <i
+                                className="fa fa-plus p-1"
+                                style={{
+                                  display: "flex",
+                                  padding: "3px",
+                                  zIndex: "0",
+                                }}
+                              ></i>
+                            </span>
+                            <span onClick={openModal}>
+                              <i
+                                className="fa fa-edit"
+                                style={{
+                                  display: "flex",
+                                  padding: "3px",
+                                  zIndex: "0",
+                                }}
+                              ></i>
+                            </span>
+                            <span onClick={openModal}>
+                              <i
+                                className="fa fa-trash"
+                                style={{
+                                  display: "flex",
+                                  padding: "3px",
+                                  zIndex: "0",
+                                }}
+                              ></i>
+                            </span>
+                          </div>
                         </button>
                       </AccordionItemButton>
                     </AccordionItemHeading>
