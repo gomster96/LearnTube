63afc7b8422f99104ae59f44bc6476deed2d758c
JuEun <moomin@handong.ac.kr>
2022-05-06 13:57:08 +0900

    [fe/feat/#36] feat: 공지 crud 구현 완료 (비동기렌더링, update수정필요)

--- frontend/src/pages/course/course-single/CurriculumPart.js
+++ frontend/src/pages/course/course-single/CurriculumPart.js
@@ -18,15 +18,29 @@ const CurriculumPart = (props) => {
   const inputRef = useRef();
   const [isOpen, setIsOpen] = useState(false);
   const openModal = () => setIsOpen(!isOpen);
+
   const [isNoticeCreateOpen, setNoticeCreateOpen] = useState(false);
   const createNotice = () => setNoticeCreateOpen(!isNoticeCreateOpen);
   const cancleCreateNotice = () => setNoticeCreateOpen(!isNoticeCreateOpen);
+
+  const initCreateBody = {
+    classId: props.classRoomData.classId,
+    title: "",
+    content: "",
+  };
+  const [createBody, setCreateBody] = useState(initCreateBody);
+
+  const [isNoticeUpdateOpen, setNoticeUpdateOpen] = useState(false);
+  const updateNotice = () => setNoticeUpdateOpen(!isNoticeUpdateOpen);
+  const cancleUpdateNotice = () => setNoticeUpdateOpen(!isNoticeUpdateOpen);
+
   const [noticeIdx, setNoticeIdx] = useState(0);
-  console.log("hello id ", props.classRoomData.classId);
+  console.log("hello classId ", props.classRoomData.classId);
   const clickModalHandler = (params) => {
     console.log("hello ", params);
     setNoticeIdx(params);
-    console.log("not: ", noticeIdx);
+    console.log("noticeIdx: ", noticeIdx);
+    console.log("notice id ", props.classRoomData.notices[noticeIdx].id);
   };
   const [noticeTitle, setNoticeTitle] = useState("");
   const [noticeContent, setNoticeContent] = useState("");
@@ -48,15 +62,14 @@ const CurriculumPart = (props) => {
 
   const noticeCreateHandler = (e) => {
     // console.log(e.target.value);
-    // state에 저장한 값을 가져옵니다.
+
     console.log(noticeTitle);
     console.log(noticeContent);
 
     let body = {
       classId: props.classRoomData.classId,
-      title: noticeContent,
+      title: noticeTitle,
       content: noticeContent,
-      //modDate:
     };
 
     axios
@@ -67,10 +80,47 @@ const CurriculumPart = (props) => {
       })
       .then((res) => console.log(res));
   };
-  useEffect(() => {
-    console.log("값 변화");
-  }, [noticeIdx]);
 
+  const noticeUpdateHandler = (e) => {
+    // console.log(e.target.value);
+
+    console.log(noticeTitle);
+    console.log(noticeContent);
+    console.log("notice id ", props.classRoomData.notices[noticeIdx].id);
+    let body = {
+      noticeId: props.classRoomData.notices[noticeIdx].id,
+      title: noticeTitle,
+      content: noticeContent,
+    };
+
+    axios
+      .post("http://localhost:8080/api/notice/update", JSON.stringify(body), {
+        headers: {
+          "Content-Type": `application/json`,
+        },
+      })
+      .then((res) => console.log(res));
+  };
+
+  const noticeDeleteHandler = (e) => {
+    // console.log(e.target.value);
+    console.log("notice id ", props.classRoomData.notices[e].id);
+    let body = {
+      noticeId: props.classRoomData.notices[e].id,
+    };
+    if (window.confirm("정말 삭제하시겠습니까?") == true) {
+      axios
+        .post("http://localhost:8080/api/notice/delete", JSON.stringify(body), {
+          headers: {
+            "Content-Type": `application/json`,
+          },
+        })
+        .then((res) => console.log(res));
+      alert("삭제되었습니다.");
+    } else {
+      return false;
+    }
+  };
   return (
     <>
       {props.classRoomData ? (
@@ -82,6 +132,7 @@ const CurriculumPart = (props) => {
             <AccordionItem className="accordion-item" uuid="a">
               <AccordionItemHeading>
                 <AccordionItemButton>
+                  {/* 공지 생성 */}
                   <Modal
                     isOpen={isNoticeCreateOpen}
                     onClose={() => {
@@ -252,6 +303,7 @@ const CurriculumPart = (props) => {
                   ? props.classRoomData.notices.map((notices, i) => (
                       <div className="content">
                         <div className="clearfix">
+                          {/* 공지 모달 open */}
                           <Modal
                             isOpen={isOpen}
                             onClose={() => {
@@ -331,10 +383,22 @@ const CurriculumPart = (props) => {
                                         </div>
                                         <br></br>
                                         <p className="text-muted">
-                                          {/* 생성일:{" "}
-                                                                                  {props[i].notices[noticeIdx].modDate.split("T")[0] +
-                                                                                      " " +
-                                                                                      props[i].notices[noticeIdx].modDate.split("T")[1]} */}
+                                          수정시간:
+                                          {props.classRoomData.notices[
+                                            noticeIdx
+                                          ].modDate.split("T")[0] +
+                                            " " +
+                                            props.classRoomData.notices[
+                                              noticeIdx
+                                            ].modDate
+                                              .split("T")[1]
+                                              .split(":")[0] +
+                                            ":" +
+                                            props.classRoomData.notices[
+                                              noticeIdx
+                                            ].modDate
+                                              .split("T")[1]
+                                              .split(":")[1]}
                                         </p>
                                         <hr></hr>
                                         <div className="row d-flex justify-content-end ms-3 me-1 mt-3">
@@ -369,11 +433,160 @@ const CurriculumPart = (props) => {
                             ></i>
                             {props.classRoomData.notices[i].title}
                           </div>
+                          {/* 공지 수정 */}
+                          <Modal
+                            isOpen={isNoticeUpdateOpen}
+                            onClose={() => {
+                              updateNotice();
+                            }}
+                            onRequestClose={() => setNoticeUpdateOpen(false)}
+                            style={{
+                              overlay: {
+                                zIndex: "100",
+                                position: "fixed",
+                                top: 0,
+                                left: 0,
+                                right: 0,
+                                bottom: 0,
+                                backgroundColor: "rgb(0, 0, 0, 0.55)",
+                              },
+                              content: {
+                                position: "absolute",
+                                top: "20%",
+                                left: "25%",
+                                right: "25%",
+                                bottom: "100px",
+                                background: "#fff",
+                                overflow: "auto",
+                                WebkitOverflowScrolling: "touch",
+                                outline: "none",
+                                padding: "0px",
+                              },
+                            }}
+                          >
+                            <div className="">
+                              <div className="register-section ">
+                                <div className="container">
+                                  <div className="py-3 px-5">
+                                    <div className="sec-title text-center mb-10">
+                                      <h2 className="title mt-3 mb-10">
+                                        공지 수정
+                                      </h2>
+                                    </div>
+                                    <div className="styled-form">
+                                      <div id="form-messages"></div>
+                                      <form
+                                        id="contact-form"
+                                        method="post"
+                                        action="#"
+                                      >
+                                        <div className="row clearfix">
+                                          <div className="form-group col-lg-12 mb-25">
+                                            <div className="my-4">
+                                              공지 제목
+                                              <span
+                                                className="ms-1"
+                                                style={{ color: "red" }}
+                                              >
+                                                *
+                                              </span>
+                                            </div>
+                                            <input
+                                              type="text"
+                                              id="title"
+                                              name="title"
+                                              value={
+                                                noticeTitle
+                                                  ? noticeTitle
+                                                  : props.classRoomData.notices[
+                                                      noticeIdx
+                                                    ].title
+                                              }
+                                              onChange={noticeTitleHandler}
+                                              placeholder="제목을 입력하세요"
+                                              required
+                                            />
+                                          </div>
+                                          <div className="form-group col-lg-12">
+                                            <div className="my-4">
+                                              공지 내용
+                                            </div>
 
+                                            <textarea
+                                              type="textarea"
+                                              id="description"
+                                              name="description"
+                                              value={
+                                                noticeContent
+                                                  ? noticeContent
+                                                  : props.classRoomData.notices[
+                                                      noticeIdx
+                                                    ].content
+                                              }
+                                              onChange={noticeContentHandler}
+                                              placeholder="설명을 입력하세요"
+                                              refs={inputRef}
+                                              style={{
+                                                position: "relative",
+                                                borderRadius: "0px",
+                                                padding: "6px 30px",
+                                                width: "100%",
+                                                color: "#222222",
+                                                fontSize: "16px",
+                                                transition: "all 500ms ease",
+                                                border: "none",
+                                                boxShadow: "0 0 30px #eee",
+                                              }}
+                                            />
+                                          </div>
+                                          {/* <div className="form-group col-lg-12">
+                                    <div className="my-2">Playlist 태그</div>
+                                    <input
+                                      type="text"
+                                      id="tag"
+                                      name="tag"
+                                      value=""
+                                      placeholder="태그를 입력하세요. 쉼표로 구분됩니다."
+                                    />
+                                  </div> */}
+                                        </div>
+                                        <div className="row d-flex justify-content-end ms-3 me-1 mt-3">
+                                          <button
+                                            type="submit"
+                                            className="canclebtn"
+                                            onClick={() => {
+                                              cancleUpdateNotice();
+                                            }}
+                                          >
+                                            <span className="txt">취소</span>
+                                          </button>
+                                          <button
+                                            type="submit"
+                                            className="createbtn text-center pt-2"
+                                            onClick={() => {
+                                              updateNotice();
+                                              noticeUpdateHandler();
+                                            }}
+                                          >
+                                            저장
+                                          </button>
+                                        </div>
+                                      </form>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </Modal>
                           <div className="pull-right">
                             <div className="minutes">
                               <div>
-                                <span onClick={createNotice}>
+                                <span
+                                  onClick={() => {
+                                    updateNotice();
+                                    clickModalHandler(i);
+                                  }}
+                                >
                                   <i
                                     className="fa fa-edit"
                                     id="editNotice"
@@ -384,7 +597,11 @@ const CurriculumPart = (props) => {
                                     }}
                                   ></i>
                                 </span>
-                                <span onClick={createNotice}>
+                                <span
+                                  onClick={() => {
+                                    noticeDeleteHandler(i);
+                                  }}
+                                >
                                   <i
                                     className="fa fa-trash"
                                     style={{
