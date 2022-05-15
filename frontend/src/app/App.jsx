beabedec6b38404a04c2da88776213f9a26d9162
JuEun <moomin@handong.ac.kr>
2022-05-15 19:27:07 +0900

    [FE/feat/#61] 구글로그인 로그인, 로그아웃 구현

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,44 +1,60 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Switch, withRouter } from 'react-router-dom';
+import React from "react";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  withRouter,
+} from "react-router-dom";
 
 //Custom Components
-import YoutubeSearch from '../pages/youtube-search';
-import DashBoard from '../pages/dashboard';
-import Main from '../pages/main';
-import Home from '../pages/home';
-import CourseOne from '../pages/course';
-import CourseSingle from '../pages/course/course-single';
-import CourseCategoryPage from '../pages/course-categories';
-import Playlist from '../pages/playlist';
-import MyPlaylist from '../pages/playlist/myPlaylist';
-import Error from '../pages/404';
-import LoadTop from '../components/Common/ScrollTop/LoadTop'
-import CreateMyPlaylist from '../pages/playlist/createMyPlaylist';
-
+import YoutubeSearch from "../pages/youtube-search";
+import DashBoard from "../pages/dashboard";
+import Main from "../pages/main";
+import Home from "../pages/home";
+import CourseOne from "../pages/course";
+import CourseSingle from "../pages/course/course-single";
+import CourseCategoryPage from "../pages/course-categories";
+import Playlist from "../pages/playlist";
+import MyPlaylist from "../pages/playlist/myPlaylist";
+import Error from "../pages/404";
+import LoadTop from "../components/Common/ScrollTop/LoadTop";
+import CreateMyPlaylist from "../pages/playlist/createMyPlaylist";
+import Login from "../pages/login";
 
 const App = () => {
-    return (
-        <div className='App'>
-            <Router>
-                <LoadTop />
-                <Switch>
-                    <Route path="/learntube-studio/youtubeSearch" component={YoutubeSearch} />
-                    <Route path="/learntube-studio" exact component={Playlist} />
-                    <Route path="/learntube-studio/myPlaylist" exact component={MyPlaylist} />
-                    <Route path="/learntube-studio/myPlaylist/create" exact component={CreateMyPlaylist} />
-                    <Route path="/dashboard" component={DashBoard} />
-                    <Route path="/" exact component={Main} />
-                    <Route path="/home" component={Home} />
-                    <Route path="/course" exact component={CourseOne} />
-                    <Route path="/course/course-single" component={CourseSingle} />
-                    <Route path="/course-categories" component={CourseCategoryPage} />
-                    <Route path="/event" component={Event} />
-                    <Route component={Error} />
-                </Switch>
-            </Router>
-
-        </div>
-    );
-}
+  return (
+    <div className="App">
+      <Router>
+        <LoadTop />
+        <Switch>
+          <Route
+            path="/learntube-studio/youtubeSearch"
+            component={YoutubeSearch}
+          />
+          <Route path="/learntube-studio" exact component={Playlist} />
+          <Route
+            path="/learntube-studio/myPlaylist"
+            exact
+            component={MyPlaylist}
+          />
+          <Route
+            path="/learntube-studio/myPlaylist/create"
+            exact
+            component={CreateMyPlaylist}
+          />
+          <Route path="/dashboard" component={DashBoard} />
+          <Route path="/" exact component={Main} />
+          <Route path="/home" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/course" exact component={CourseOne} />
+          <Route path="/course/course-single" component={CourseSingle} />
+          <Route path="/course-categories" component={CourseCategoryPage} />
+          <Route path="/event" component={Event} />
+          <Route component={Error} />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
