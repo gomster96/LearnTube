6669ac59b794c182a69784aac47b70a93637f328
Byungwoong An <80206884+gomster96@users.noreply.github.com>
2022-05-19 20:59:30 +0900

    feat[#73] : 수강중인 학생 목록 조회 API 구현 (#79)

--- backend/src/main/java/com/walab/classroom/domain/repository/ClassRoomRepository.java
+++ backend/src/main/java/com/walab/classroom/domain/repository/ClassRoomRepository.java
@@ -23,16 +23,4 @@ public interface ClassRoomRepository extends JpaRepository<ClassRoom, Long>, Cla
             "where c.id = :classId")
     Optional<ClassRoom> findFirstByClassById(@Param("classId") Long classId);
 
-    @Query("select c from ClassRoom  c " +
-            "left join fetch c.takes " +
-            "join fetch c.instructor " +
-            "where c.isActive = true " +
-            "order by c.createdAt desc ")
-    List<ClassRoom> findByPage(Pageable pageable);
-
-    @Query("select c " +
-            "from ClassRoom  c, Lecture  l " +
-            "left join fetch c.lectures " +
-            "where l.classRoom.id = c.id ")
-    Optional<ClassRoom> findFirstByLectureId(@Param("lectureId") Long lectureId);
 }
