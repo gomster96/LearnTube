c1e9ef02d941c29fcdbac0df6e13cb78ebe4d252
gomster <gomster96@gmail.com>
2022-05-20 01:26:09 +0900

    feat[#74] : 프론트엔드 페이징 적용

--- frontend/src/pages/course/CourseSection.js
+++ frontend/src/pages/course/CourseSection.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import axios, * as others from "axios";
 
 import CourseSidebar from "./CourseSidebarSection";
 import "../../assets/css/courseList.css";
 import CourseSingleTwoCopy from "../../components/Courses/CourseSingleTwoCopy";
 
-// Course courseImg
 import courseImg1 from "../../assets/img/courses/1.jpg";
 import SearchBar from "./SearchBar";
+import PagingBar from "./PagingBar";
 
 const CoursePart = (props) => {
     const [courses, setCourse] = useState([]);
@@ -49,7 +48,6 @@ const CoursePart = (props) => {
                             </div>
                         </div>
                     </div>
-                    {/* {renderCourses} */}
                     {courses
                         ? courses.map((course) => {
                               return (
@@ -68,24 +66,7 @@ const CoursePart = (props) => {
                               );
                           })
                         : null}
-
-                    <div className="pagination-area orange-color text-center mt-30 md-mt-0">
-                        <ul className="pagination-part">
-                            <li className="active">
-                                <Link to="#">1</Link>
-                            </li>
-                            <li>
-                                <Link to="#">2</Link>
-                            </li>
-                            <li>
-                                <Link to="#">
-                                    Next <i className="fa fa-long-arrow-right"></i>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-
-                    {/* </div> */}
+                    {courses ? <PagingBar page={filterStatus.page} totalPage={courses.length !== 0 ? courses[0].totalPage : 0} setFilterStatus={setFilterStatus} /> : null}
                 </div>
             </div>
         </div>
