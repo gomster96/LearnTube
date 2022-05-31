7854687a382cc227eb90bd5c433ede1443a918c8
JuEun <moomin@handong.ac.kr>
2022-05-31 22:49:14 +0900

    로그인 에러해결 및 콘솔 삭제 후 배포버전

--- frontend/src/pages/dashboard/CourseMain.js
+++ frontend/src/pages/dashboard/CourseMain.js
@@ -4,18 +4,18 @@ import ScrollToTop from "../../components/Common/ScrollTop";
 import { useHistory } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 function CourseMain(props) {
-    console.log("test" + props.userId);
-    return (
-        <React.Fragment>
-            {/* CoursePart Start */}
-            <CoursePart userId={props.userId} />
-            {/* CoursePart End */}
+  // console.log("test" + props.userId);
+  return (
+    <React.Fragment>
+      {/* CoursePart Start */}
+      <CoursePart userId={props.userId} />
+      {/* CoursePart End */}
 
-            {/* scrolltop-start */}
-            <ScrollToTop scrollClassName="scrollup orange-color" />
-            {/* scrolltop-end */}
-        </React.Fragment>
-    );
+      {/* scrolltop-start */}
+      <ScrollToTop scrollClassName="scrollup orange-color" />
+      {/* scrolltop-end */}
+    </React.Fragment>
+  );
 }
 
 export default CourseMain;
--- frontend/src/pages/dashboard/index.js
+++ frontend/src/pages/dashboard/index.js
@@ -18,58 +18,61 @@ import Logo from "../../assets/img/logo/Learntube-logos_transparent.png";
 import footerLogo from "../../assets/img/logo/lite-logo.png";
 
 const CourseOne = (props) => {
-    const userId = window.sessionStorage.getItem("userId");
-    let tab1 = "수강중인 강의실",
-        tab2 = "관리중인 강의실",
-        tab3 = "종료된 강의실",
-        tabStyle = "intro-tabs tabs-box";
-    return (
-        <React.Fragment>
-            <OffWrap />
-            <Header
-                parentMenu="dashboard"
-                headerNormalLogo={Logo}
-                headerStickyLogo={Logo}
-                CanvasLogo={Logo}
-                mobileNormalLogo={Logo}
-                CanvasClass="right_menu_togle hidden-md"
-                headerClass="full-width-header header-style1 home8-style4"
-            />
+  const userId = window.sessionStorage.getItem("userId");
+  let tab1 = "수강중인 강의실",
+    tab2 = "관리중인 강의실",
+    tab3 = "종료된 강의실",
+    tabStyle = "intro-tabs tabs-box";
+  return (
+    <React.Fragment>
+      <OffWrap />
+      <Header
+        parentMenu="dashboard"
+        headerNormalLogo={Logo}
+        headerStickyLogo={Logo}
+        CanvasLogo={Logo}
+        mobileNormalLogo={Logo}
+        CanvasClass="right_menu_togle hidden-md"
+        headerClass="full-width-header header-style1 home8-style4"
+      />
 
-            <div class="container text-center dashboard-tabs">
-                <div className="intro-info-tabs border-none">
-                    <Tabs>
-                        <TabList className={tabStyle}>
-                            <Tab>
-                                <button>{tab1}</button>
-                            </Tab>
-                            <Tab>
-                                <button>{tab2}</button>
-                            </Tab>
-                            <Tab>
-                                <button>{tab3}</button>
-                            </Tab>
-                        </TabList>
+      <div class="container text-center dashboard-tabs">
+        <div className="intro-info-tabs border-none">
+          <Tabs>
+            <TabList className={tabStyle}>
+              <Tab>
+                <button>{tab1}</button>
+              </Tab>
+              <Tab>
+                <button>{tab2}</button>
+              </Tab>
+              <Tab>
+                <button>{tab3}</button>
+              </Tab>
+            </TabList>
 
-                        <TabPanel>
-                            <CourseMain userId={userId} />
-                        </TabPanel>
+            <TabPanel>
+              <CourseMain userId={userId} />
+            </TabPanel>
 
-                        <TabPanel>
-                            <CourseMainAdmin userId={userId} />
-                        </TabPanel>
+            <TabPanel>
+              <CourseMainAdmin userId={userId} />
+            </TabPanel>
 
-                        <TabPanel>
-                            <CourseMainClosed userId={userId} />
-                        </TabPanel>
-                    </Tabs>
-                </div>
-            </div>
+            <TabPanel>
+              <CourseMainClosed userId={userId} />
+            </TabPanel>
+          </Tabs>
+        </div>
+      </div>
 
-            <Footer footerClass="rs-footer home9-style main-home" footerLogo={footerLogo} />
-            <SearchModal />
-        </React.Fragment>
-    );
+      <Footer
+        footerClass="rs-footer home9-style main-home"
+        footerLogo={footerLogo}
+      />
+      <SearchModal />
+    </React.Fragment>
+  );
 };
 
 export default CourseOne;
